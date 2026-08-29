bd8096330bbb33b7dc50a8ac9dda75491c97bdbb
RajPopat-hash <rajraghuvanshi7706@gmail.com>
2026-08-29 23:42:36 +0530

    Add basic arithmetic operations in P1.py

---
+++ P1.py
@@ -0,0 +1,15 @@
+# Take two numbers from the user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Mathematical operations
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+# Display results
+print("Addition =", addition)
+print("Subtraction =", subtraction)
+print("Multiplication =", multiplication)
+print("Division =", division)
