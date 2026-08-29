93bcf35a2c6e268b972fe8f685955c043c6313c2
RajPopat-hash <rajraghuvanshi7706@gmail.com>
2026-08-29 23:46:24 +0530

    Implement swap function for two numbers

---
+++ P7.py
@@ -0,0 +1,12 @@
+def swap(a, b):
+    a, b = b, a
+    return a, b
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+a, b = swap(a, b)
+
+print("After swapping:")
+print("First number =", a)
+print("Second number =", b)
